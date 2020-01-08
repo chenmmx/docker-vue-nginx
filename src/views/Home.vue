a363710c2ad80a2cfdc6372ed5e13557e4a1ef6f
chenmmx <1107204891@qq.com>
2020-01-08 10:28:11 +0800

    init project

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>你就是一个哈皮</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: "home",
+  data() {
+    return {
+      name: "",
+      age: false
+    };
+  },
+  components: {
+    // HelloWorld
+  }
+};
+</script>
